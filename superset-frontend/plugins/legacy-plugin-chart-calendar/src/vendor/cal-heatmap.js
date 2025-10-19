522951af373c561f57ad978651a4441ede6ae5a7
Longqirui Chen <longqirui.chen@mail.utoronto.ca>
2025-10-19 19:40:22 -0400

    Base

--- superset-frontend/plugins/legacy-plugin-chart-calendar/src/vendor/cal-heatmap.js
+++ superset-frontend/plugins/legacy-plugin-chart-calendar/src/vendor/cal-heatmap.js
@@ -2727,7 +2727,8 @@ CalHeatMap.prototype = {
       stop = new Date(start);
       stop = stop.setMonth(stop.getMonth() + range);
     }
-
+    console.log("######hello world");
+    console.log("######start, stop", start, new Date(stop));
     return d3.time.months(Math.min(start, stop), Math.max(start, stop));
   },
 
